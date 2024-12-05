7d30a8297ccec3fc8ea36344f385f827fb1f0b43
JonasPorpino <49802323+JonasPorpino@users.noreply.github.com>
2024-12-04 21:44:40 -0300

    views

--- CoisasEmDjango/CoisasEmDjango/urls.py
+++ CoisasEmDjango/CoisasEmDjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#from django.urls import url
+from .views import home, clientes
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'clientes/',clientes),
+    path(r'',home),
 ]
