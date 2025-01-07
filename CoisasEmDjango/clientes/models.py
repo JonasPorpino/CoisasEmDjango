69d92f031e541e76ea8bd3471f57f3ff3654ead4
jonas <49802323+JonasPorpino@users.noreply.github.com>
2025-01-07 19:01:00 -0300

    registro de model admin

--- CoisasEmDjango/clientes/models.py
+++ CoisasEmDjango/clientes/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=70, null=False)
     endereco = models.CharField(max_length=200, blank=False, null=False)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    
