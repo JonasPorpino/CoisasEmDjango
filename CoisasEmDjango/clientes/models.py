56f09ad585152c893cb69668e8d252bad82b8d33
jonas <49802323+JonasPorpino@users.noreply.github.com>
2025-01-11 20:14:03 -0300

    tabela de telefones
    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, default=1)
    
    Aqui, default=1 define que o cliente com ID 1 será associado por padrão. Certifique-se de que esse ID exista no banco de dados para evitar erros de integridade referencial.

--- CoisasEmDjango/clientes/models.py
+++ CoisasEmDjango/clientes/models.py
@@ -17,7 +17,7 @@ class Cliente(models.Model):
 class Telefone(models.Model):
     numero = models.CharField(max_length=20)
     descricao = models.CharField(max_length=80)
-    cliente = models.ForeignKey(Cliente)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.descricao + ' - ' + self.numero
