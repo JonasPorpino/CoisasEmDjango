b73103a9242a7abc317f35407d13592e41317216
jonas <49802323+JonasPorpino@users.noreply.github.com>
2025-01-13 16:39:09 -0300

    Up
    Essa parte esta com erro na migração do cpf

--- CoisasEmDjango/clientes/admin.py
+++ CoisasEmDjango/clientes/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Cliente, Telefone
+from .models import Cliente, Telefone, CPF
 
 admin.site.register(Cliente)
 admin.site.register(Telefone)
+admin.site.register(CPF)
 
 # Register your models here.
--- CoisasEmDjango/clientes/models.py
+++ CoisasEmDjango/clientes/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+# o tipo de relacionamento é de um para um
+class CPF(models.Model):
+    numero = models.CharField(max_length=11)
+    data_exp = models.DateTimeField(auto_now=False)
+
 # Create your models here.
 
 class Cliente(models.Model):
@@ -8,6 +13,7 @@ class Cliente(models.Model):
     salario = models.DecimalField(max_digits=10, decimal_places=2)
     idade = models.IntegerField()
     email = models.EmailField()
+    cpf = models.OneToOneField(CPF, on_delete=models.CASCADE) # ele vem acima por questão de referência
 
     def __str__(self):
         return self.nome
@@ -22,3 +28,4 @@ class Telefone(models.Model):
 
     def __str__(self):
         return self.descricao + ' - ' + self.numero
+    
