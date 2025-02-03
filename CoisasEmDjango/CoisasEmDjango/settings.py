1b5f51810a4c4ff62d793204b86c3506b9f9d11b
jonas <49802323+JonasPorpino@users.noreply.github.com>
2025-02-03 16:34:26 -0300

    acesso pela rede local

--- CoisasEmDjango/CoisasEmDjango/settings.py
+++ CoisasEmDjango/CoisasEmDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1-r8_+=89*$ef^y9r)^7fk+k8o66bq9(^$*!@e$m(pmmwki##@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.104']
 
 
 # Application definition
