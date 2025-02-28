15e0931746b43d62ece785d38816437c57468664
jonas <49802323+JonasPorpino@users.noreply.github.com>
2025-02-27 21:09:22 -0300

    up
    definido templats e estrutura de decisão

--- CoisasEmDjango/CoisasEmDjango/views.py
+++ CoisasEmDjango/CoisasEmDjango/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # existem dois tipos de views  as:
 # function based views e as class based views
 # aqui apenas functions
 
 def home(request):
-    return HttpResponse('Ola mundo')
+    sexo = 'm'
+    nome = 'Mendonça'
+    return render(request,'index.html', {'sexo': sexo, 'nome': nome})
+
 
